80d988280c1bd4def22564d6def626a54fc57b4b
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2023-07-25 22:37:29 +1200

    Convert to wasm Blazor project

--- FifaTeamMaker/Program.cs
+++ FifaTeamMaker/Program.cs
@@ -1,28 +1,11 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using FifaTeamMaker.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FifaTeamMaker;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+await builder.Build().RunAsync();
